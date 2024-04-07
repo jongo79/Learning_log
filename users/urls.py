58e8bf8da551890848dfdd0ee0407e386cd4445e
jongo79 <jongfad@gmail.com>
2024-04-07 17:32:02 +0200

    Users App created

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+"""Defines URL patterns for users"""
+from django.urls import path
+from django.contrib.auth.views import LoginView  # Import LoginView
+from . import views
+
+app_name = 'users'  # Add this line to specify the app name
+
+urlpatterns = [
+    # Login page
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+
+]
