4795207272f0f029b0fcf848e97cc975c872b65a
jongo79 <jongfad@gmail.com>
2024-04-25 20:27:20 +0200

    Deployed locally on 127.0.0.1

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -29,14 +30,12 @@ DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
 
 # Define the default value
 DEFAULT_ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split() or DEFAULT_ALLOWED_HOSTS
 
-# Check if the ALLOWED_HOSTS environment variable is set
-# If set, split the value by spaces and use it as ALLOWED_HOSTS
-# Otherwise, use the default value
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split() or ['localhost', '127.0.0.1']
 
 
 # Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -47,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'learning_logs',
     'users',
-    # Third party apps.
     'django_bootstrap5',
 ]
 
@@ -85,16 +83,19 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ['DATABASE_URL'])
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
-database_url = os.environ.get("DATABASE_URL")
-
-DATABASES['default'] = dj_database_url.parse(database_url)
 
 
 # Password validation
