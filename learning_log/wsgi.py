d9748a5927c98a837177329442030e69eeaa7c6e
jongo79 <162108395+jongo79@users.noreply.github.com>
2024-04-26 09:10:10 +0200

    Update wsgi.py back

--- learning_log/wsgi.py
+++ learning_log/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning_logs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning_log.settings')
 
 application = get_wsgi_application()
