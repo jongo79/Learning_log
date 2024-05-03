46fd5788818d19c8f478b3896b0bc7cfeabf28b1
jongo79 <jongfad@gmail.com>
2024-05-03 12:30:54 +0200

    superuser emv vars and if statement

--- build.sh
+++ build.sh
@@ -11,3 +11,8 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py makemigrations
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
+fi
